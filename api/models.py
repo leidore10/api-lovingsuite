862cd0f136f09db8579c568676dc188a0131508a
rodieldv <rodieldev@yahoo.com>
2021-07-04 03:06:47 -0400

    add Model  User

--- api/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib import admin
-from django.core.validators import ValidationError
-from django.db.models.base import ModelBase, subclass_exception
-from django.contrib.auth.models import User
-from django.apps.registry import apps
-from datetime import datetime
-
-# Create your models here.
--- delete_migrations.sh
+++
@@ -1,2 +0,0 @@
-sudo find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
-sudo find . -path "*/migrations/*.pyc"  -delete
